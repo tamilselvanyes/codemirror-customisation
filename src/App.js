6c190cea3d67b95f3ad43d3daf9bfd680bf6c546
tamilselvanyes <tamilselvanyes@gmail.com>
2023-06-27 22:56:30 +0530

    code mirror changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import { EditorView, basicSetup } from "@codemirror/basic-setup";
+import { sql } from "@codemirror/lang-sql";
+import { showHint, sqlHint } from "@codemirror/sql-hint";
+
+import "@codemirror/basic-setup/lib/basic-setup.css";
+import "@codemirror/lang-sql/sql.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const editorRef = useRef(null);
+
+  useEffect(() => {
+    const editor = new EditorView({
+      state: EditorState.create({
+        doc: "",
+        extensions: [basicSetup, sql(), showHint(), sqlHint()],
+      }),
+      parent: editorRef.current,
+    });
+
+    // Register a hint function to provide suggestions
+    editor.contentDOM.addEventListener("keydown", (event) => {
+      if (event.key === " ") {
+        showHint({
+          hint: sqlHint,
+        })(editor);
+      }
+    });
+  }, []);
+
+  return <div ref={editorRef} />;
 }
 
 export default App;
